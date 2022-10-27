5f691e114ee78213de1505cf5439e1b8c6b970dd
AlexPBradley <apbradley@hotmail.com>
2022-10-27 19:30:55 +0100

    first name, last name, likes and dislikes added

--- src/models/user.js
+++ src/models/user.js
@@ -1,76 +1,76 @@
 module.exports = (connection, DataTypes) => {
-    const schema = {
-      first_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notNull: {
-            args: [true],
-            msg: "We need a first name",
-          },
-          notEmpty: {
-            args: [true],
-            msg: "We need a first name",
-          },
+  const schema = {
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'We need a first name',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'We need a first name',
         },
       },
-      last_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notNull: {
-            args: [true],
-            msg: "We need a last name",
-          },
-          notEmpty: {
-            args: [true],
-            msg: "We need a last name",
-          },
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'We need a last name',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'We need a last name',
         },
       },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notNull: {
-            args: [true],
-            msg: "We need an email",
-          },
-          notEmpty: {
-            args: [true],
-            msg: "We need an email",
-          },
-          isEmail: {
-            args: [true],
-            msg: "email is not valid",
-          },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'We need an email',
+        },
+        notEmpty: {
+          args: [true],
+          msg: 'We need an email',
+        },
+        isEmail: {
+          args: [true],
+          msg: 'email is not valid',
         },
       },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          notNull: {
-            args: [true],
-            msg: "We need a password",
-          },
-          notEmpty: {
-            args: [true],
-            msg: "We need a password",
-          },
-          len: [8, 99],
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          args: [true],
+          msg: 'We need a password',
         },
+        notEmpty: {
+          args: [true],
+          msg: 'We need a password',
+        },
+        len: [8, 99],
       },
-      likes: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      dislikes: {
-        type: DataTypes.STRING,
-        allowNull: true
-      }
-    };
-  
-    const UserModel = connection.define("User", schema);
-    return UserModel;
+    },
+    likes: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    dislikes: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
   };
+
+  const UserModel = connection.define('User', schema);
+  return UserModel;
+};
