b1c1d92e2ab71258e6545d160cceee96bb13f91b
Lisa Heffernan <lisaheffernan@outlook.com>
2022-10-27 21:17:07 +0100

    another cors fix

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const userRouter = require("../router/user");
 
 const app = express();
@@ -8,6 +9,9 @@ app.use((_, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
+
+app.use(cors({ credentials: true, origin: true }));
+app.options("*", cors());
 
 app.use(express.json());
 app.use("/users", userRouter);
