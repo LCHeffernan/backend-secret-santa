0bdfb4ddf15014bdc659e8266ce662dba693b1a5
AlexPBradley <apbradley@hotmail.com>
2022-10-27 18:50:13 +0100

    users crud operations setup

--- controller/user.js
+++ controller/user.js
@@ -1,11 +1,29 @@
-const { User } = require("../src/models");
+const { User } = require('../src/models');
 
 exports.findUser = async (_, res) => {
-    const allUser = await User.findAll();
-    res.status(200).json(allUser);
+  const allUser = await User.findAll();
+  res.status(200).json(allUser);
 };
 
+exports.findUserById = async (req, res) => {
+    const { id } = req.params;
+    const allUser = await User.findAll({ where: { id: id } });
+    res.status(200).json(allUser);
+  };
+
 exports.createUser = async (req, res) => {
-    const newUser = await User.create(req.body);
-    res.status(201).json(newUser);
+  const newUser = await User.create(req.body);
+  res.status(201).json(newUser);
+};
+
+exports.updateUserById = async (req, res) => {
+  const { id } = req.params;
+  const userUpdate = await User.update(req.body, { where: { id: id } });
+  res.status(200).json(userUpdate);
+};
+
+exports.deleteUserById = async (req, res) => {
+  const { id } = req.params;
+  const userDeleted = await User.destroy({ where: { id: id } });
+  res.status(200).json(userDeleted);
 };
--- router/user.js
+++ router/user.js
@@ -4,7 +4,10 @@ const userController = require("../controller/user");
 const userRouter = express.Router();
 
 userRouter.get("/", userController.findUser);
+userRouter.get("/:id", userController.findUserById);
 userRouter.post("/", userController.createUser);
+userRouter.patch("/:id", userController.updateUserById);
+userRouter.delete("/:id", userController.deleteUserById);
 
 
 module.exports = userRouter;
