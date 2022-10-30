3767c8e6d00a51687de62e0437b01964ae4ddf1b
Suzannah Barnes <smgarland@hotmail.co.uk>
2022-10-30 15:31:14 +0000

    updated event.routes

--- tests/event.test.js
+++ tests/event.test.js
@@ -1,5 +1,25 @@
-const { Event } = require("../models");
+// const { expect } = require("chai");
+// const request = require('supertest');
+// const { Event } = require("../models");
+// const app = require("../index")
 
-describe("/events", () => {
+// const { Event } = require("../models");
 
-})
+// describe("/events", () => {
+//     before(async () => Event.sequelize.sync())
+//     beforeEach(async () => {
+//         await Event.destory({ where: {} })
+//     })
+
+//     describe("with no records in the database", () => {
+//         describe("POST /events", () => {
+//             it("creates a new reader in the database", async () => {
+//                 const response = await (await request(app).post('/events')).setEncoding({
+//                     title: "MCR Codes Xmas",
+//                     exchange_date:
+//                     budget:
+//                 })
+//             })
+//         })
+//     })
+// })
