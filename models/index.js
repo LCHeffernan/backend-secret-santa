170442b2ec2cc2c5c7ed89efaf99ab4dd6ed74cf
Lisa Heffernan <lisaheffernan@outlook.com>
2022-10-30 20:39:08 +0000

    think the user_event table is associated correctly

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
-const Sequelize = require("sequelize");
-const UserModel = require("./user");
-const EventModel = require("./event")
+const Sequelize = require('sequelize');
+const UserModel = require('./user');
+const EventModel = require('./event');
+const UserEventsModel = require('./userEvents');
 
 const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
 
@@ -8,12 +9,24 @@ const setUpDatabase = () => {
   const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres",
+    dialect: 'postgres',
     logging: false,
   });
 
   const User = UserModel(connection, Sequelize);
   const Event = EventModel(connection, Sequelize);
+  const UserEvents = UserEventsModel(connection, Sequelize);
+
+  // User.belongsToMany(Event, { foreighKey: 'userId', as: 'userId', through: { model: 'UserEvents', unique: false}});
+  // User.belongsToMany(Event, { foreignKey: 'buyForId', as: 'buyForId', through: { model: 'UserEvents', unique: false }});
+  // Event.belongsToMany(User, { through: 'UserEvents' });
+
+  User.hasMany(UserEvents);
+  UserEvents.belongsTo(User);
+  User.hasMany(UserEvents);
+  UserEvents.belongsTo(User, { as: "BuyForId" });
+  Event.hasMany(UserEvents);
+  UserEvents.belongsTo(Event);
 
   connection.sync({ alter: true });
 
@@ -21,7 +34,6 @@ const setUpDatabase = () => {
     User,
     Event,
   };
-
 };
 
-module.exports = setUpDatabase()
+module.exports = setUpDatabase();
---
+++ models/userEvents.js
@@ -0,0 +1,31 @@
+module.exports = (connection, DataTypes) => {
+    const schema = {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+        // references: {
+        //     model: "Users",
+        //     key: "id"
+        // }
+      }
+    //   eventId: {
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //         model: "Events",
+    //         key: "id"
+    //     }
+    //   }, 
+    //   buyForId: {
+    //     type: DataTypes.INTEGER,
+    //     references: {
+    //         model: "Users",
+    //         key: "id"
+    //     }
+    //   }
+    };
+  
+    const UserEventsModel = connection.define('UserEvents', schema);
+    return UserEventsModel;
+  };
